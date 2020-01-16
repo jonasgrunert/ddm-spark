24e8618ddad802b22ba61fb26a289139cdf9a067
Jonas Grunert <jonas@grunert-berlin.de>
2020-01-16 17:34:21 +0100

    Some further upgrades
    Now should read in all the data from region and country

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -5,7 +5,15 @@ import org.apache.spark.sql.{Dataset, Row, SparkSession}
 object Sindy {
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
+    import spark.implicits._
 
-    // TODO
+    val dataframes=inputs.map(path=>spark.read
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .option("delimiter", ";")
+      .csv(path)
+    )
+
+    //TODO 
   }
 }
