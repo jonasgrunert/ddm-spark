782384563292fd6c88165036dca2aa7573666773
jonasgrunert <jonas@grunert-berlin.de>
2020-01-16 17:50:59 +0100

    Adding types in Sindy

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,19 +1,19 @@
 package de.hpi.spark_tutorial
 
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 object Sindy {
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
     import spark.implicits._
 
-    val dataframes=inputs.map(path=>spark.read
+    val dataframes: List[DataFrame] = inputs.map(path=>spark.read
       .option("inferSchema", "true")
       .option("header", "true")
       .option("delimiter", ";")
       .csv(path)
     )
 
-    //TODO 
+    //TODO Implement strategy to work with it
   }
 }
