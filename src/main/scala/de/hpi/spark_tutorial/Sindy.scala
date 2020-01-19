689064fd13299734455a653ddd111d856d9c81df
jonasgrunert <jonas@grunert-berlin.de>
2020-01-19 15:00:55 +0100

    Implementing Figure 1

--- src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -7,12 +7,46 @@ object Sindy {
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
     import spark.implicits._
 
+    // Reading the data
     val dataframes: List[DataFrame] = inputs.map(path=>spark.read
       .option("inferSchema", "true")
       .option("header", "true")
       .option("delimiter", ";")
       .csv(path)
     )
+
+    // ####################################################################### Figure 1
+
+    // Step One: Creating tuples of (value, column_name) e.g. (AFRICA, R_NAME)
+    val candidatesTuples = dataframes
+      .map(df => {
+        // Column Names as Sets so we can use Set operations later on
+        val columns = df.columns.map(Set(_))
+        // return is implicit
+        df
+          .rdd
+          // The zip operator applies one column name repeatedly to one value in a row
+          .flatMap(row => row.toSeq.map(_.toString) zip columns)
+          // Step Two: For every same value collect all columnnames (value, columns_names)
+          // e.g. (0, {N_NATIONKEY, N_REGIONKEY, R_REGIONKEY)
+          // That groups them by _1, so same values together
+          .groupByKey
+          // that maps on each value once and combines them on a table_basis
+          .map(group => {
+            //Takes in a tuple of (String, multiple Sets) and combines them into (String, all unique values from all Sets)
+            (group._1, group._2.reduce(_++_))
+          })
+          .toDS
+      })
+      // Step Three: We partition globally
+      .reduce(_ union _)
+      .rdd
+      .groupByKey
+      // Step Four: We build only attribute Sets
+      .map(_._2.reduce(_++_))
+      .toDS
+
+    // ####################################################################### Figure 2
 
     //TODO Implement strategy to work with it
   }
